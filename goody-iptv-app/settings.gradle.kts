7d69179e889e71e4d9d54cd4730f891552614fb1
oduameh <emmanuel.ameh@iohk.io>
2025-08-10 07:54:54 +0100

    Fix: add Google Services plugin versions to unblock Android CI

--- goody-iptv-app/settings.gradle.kts
+++ goody-iptv-app/settings.gradle.kts
@@ -7,6 +7,8 @@ pluginManagement {
     plugins {
         id("com.android.application") version "8.4.1"
         id("org.jetbrains.kotlin.android") version "1.9.24"
+        id("com.google.gms.google-services") version "4.4.2"
+        id("com.google.firebase.crashlytics") version "3.0.2"
     }
 }
 dependencyResolutionManagement {
